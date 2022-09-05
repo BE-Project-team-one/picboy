660a7e6ab766c8a6aae0608224266800075c028e
dongkeon-lee <veha357@naver.com>
2022-09-06 00:18:04 +0900

    commit8

---
+++ src/main/java/com/sparta/picboy/service/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.sparta.picboy.service;
+
+import com.sparta.picboy.domain.Member;
+import com.sparta.picboy.domain.UserDetailsImpl;
+import com.sparta.picboy.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+  private final MemberRepository memberRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    Optional<Member> member = memberRepository.findByNickname(username);
+    return member
+        .map(UserDetailsImpl::new)
+        .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+  }
+}
