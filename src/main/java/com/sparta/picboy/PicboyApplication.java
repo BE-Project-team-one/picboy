68b2b4ec787bdff56080e441104ddf319c37c1b0
dongkeon-lee <veha357@naver.com>
2022-09-05 23:20:53 +0900

    첫커밋

---
+++ src/main/java/com/sparta/picboy/PicboyApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.picboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicboyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicboyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/picboy/PicboyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.picboy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicboyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
