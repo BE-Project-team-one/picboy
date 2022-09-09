08a814d5a489aad98b034357b3ab6a78f50ebe86
sunho <yalla12@naver.com>
2022-09-09 14:15:25 +0900

    [update] 알람 dto 상태값 추가

--- src/main/java/com/sparta/picboy/dto/response/post/AlertResponseDto.java
+++ src/main/java/com/sparta/picboy/dto/response/post/AlertResponseDto.java
@@ -9,10 +9,12 @@ public class AlertResponseDto {
     private Long postId;
     private String content;
     private String receiver;
+    private boolean flag; // true이면 메시지가 정상적으로 전달됨
 
     public AlertResponseDto(Long postId, String content, String receiver) {
         this.postId = postId;
         this.content = content;
         this.receiver = receiver;
+        this.flag = true;
     }
 }
