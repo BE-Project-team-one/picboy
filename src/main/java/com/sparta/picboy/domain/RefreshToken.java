69f1da71cb426522684f7c3d5c9f443aca3403b3
dongkeon-lee <veha357@naver.com>
2022-09-09 15:24:19 +0900

    카스케이드

--- src/main/java/com/sparta/picboy/domain/RefreshToken.java
+++ src/main/java/com/sparta/picboy/domain/RefreshToken.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 
@@ -21,6 +23,7 @@ public class RefreshToken extends Timestamped {
 
   @JoinColumn(name = "member_id", nullable = false)
   @OneToOne(fetch = FetchType.LAZY)
+  @OnDelete(action = OnDeleteAction.CASCADE)
   private Member member;
 
   @Column(nullable = false)
