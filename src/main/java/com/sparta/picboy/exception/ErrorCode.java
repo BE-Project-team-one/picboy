2d57a92a5672406ef6b3ca2d47b64f0531dcbdae
dongkeon-lee <veha357@naver.com>
2022-09-14 14:53:44 +0900

    용욱,창균 에러코드 병합

--- src/main/java/com/sparta/picboy/exception/ErrorCode.java
+++ src/main/java/com/sparta/picboy/exception/ErrorCode.java
@@ -1,6 +1,5 @@
 package com.sparta.picboy.exception;
 
-import com.sparta.picboy.dto.response.ResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -16,15 +15,10 @@ public enum ErrorCode {
     ALREADY_COMPLETED_POST(400, "POST-ERR-400", "이미 완료된 게시물입니다."),
     INVALID_VALUE(400, "PASSWORD-ERR-400", "패스워드가 다릅니다."),
     ONLY_AUTHOR_ACCESSIBLE(400,"POST-ERR-400", "작성자만 접근할 수 있습니다."),
-    NOT_FOUNT_COMMENT(400, "POST-ERR-400", "댓글을 찾을 수 업습니다."),
+    NOT_FOUNT_COMMENT(400, "POST-ERR-400", "댓글을 찾을 수 없습니다."),
     UNAUTHORIZED (401, "LOGIN-ERR-401", "로그인이 필요합니다.(권한 없음)"),
     FORBIDDEN(401,"LOGIN-ERR-401", "로그인이 필요합니다.(FORBIDDEN)"),
     CERTIFICATION_NOT_MATCH(400, "CERTIFICATION-ERR-400", "인증 번호가 일치 하지 않습니다."),
-    ALREADY_EXIST_USERNAME(400,"MEMBER-ERR-400", "이미 존재하는 아이디입니다."),
-    ALREADY_EXIST_NICKNAME(400,"MEMBER-ERR-400", "이미 존재하는 닉네임입니다.")
-    NOT_FOUNT_COMMENT(400, "POST-ERR-400", "댓글을 찾을 수 업습니다."),
-
-    //
     ALREADY_EXIST_USERNAME(400,"MEMBER-ERR-400", "이미 존재하는 아이디입니다."),
     ALREADY_EXIST_NICKNAME(400,"MEMBER-ERR-400", "이미 존재하는 닉네임입니다."),
     NOT_CORRECT_PASSWORD(400,"MEMBER-ERR-400", "비밀번호가 일치하지 않습니다."),
