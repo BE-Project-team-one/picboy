e84a0a924c848b7c0c249c557344d1bb41d1784b
sunho <yalla12@naver.com>
2022-09-20 23:40:50 +0900

    [update] 재배포

--- src/main/java/com/sparta/picboy/WebSocket/WebSocketConfig.java
+++ src/main/java/com/sparta/picboy/WebSocket/WebSocketConfig.java
@@ -23,6 +23,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+
         registry.enableSimpleBroker("/sub");
         registry.setApplicationDestinationPrefixes("/pub");
     }
