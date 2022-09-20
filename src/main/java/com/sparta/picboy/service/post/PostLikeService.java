ac2d5eb616a00cd6e5442f620104debac2f4f333
dongkeon-lee <veha357@naver.com>
2022-09-20 21:03:30 +0900

    [update] 서버재부팅

--- src/main/java/com/sparta/picboy/service/post/PostLikeService.java
+++ src/main/java/com/sparta/picboy/service/post/PostLikeService.java
@@ -40,6 +40,7 @@ public class PostLikeService {
         if (postCheck.isEmpty()) { // 게시물이 존재하지 않은경우
             return ResponseDto.fail(ErrorCode.NOT_FOUNT_POST);
         }
+
         Post post = postRepository.findById(postid).orElseThrow();
 
 
