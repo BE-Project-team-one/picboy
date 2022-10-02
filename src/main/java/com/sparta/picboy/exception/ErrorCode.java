90c1053e3391e36ef6b7054e5ef711bf4b833337
dongkeon-lee <veha357@naver.com>
2022-10-02 21:56:53 +0900

    [update] 에러코드 오타 수정

--- src/main/java/com/sparta/picboy/exception/ErrorCode.java
+++ src/main/java/com/sparta/picboy/exception/ErrorCode.java
@@ -9,7 +9,7 @@ public enum ErrorCode {
     NOT_FOUND(404,"COMMON-ERR-404","PAGE NOT FOUND"),
     INTER_SERVER_ERROR(500,"COMMON-ERR-500","INTER SERVER ERROR"),
     NOT_FOUND_MEMBER(500, "MEMBER-ERR-400", "유저를 찾을 수 없습니다."),
-    NOT_FOUNT_POST(500, "POST-ERR-400", "게시물을 찾을 수 업습니다."),
+    NOT_FOUNT_POST(500, "POST-ERR-400", "게시물을 찾을 수 없습니다."),
     FAIL_FILE_UPLOAD(500, "FILE-ERR-400","파일 업로드를 실패했습니다."),
     NOT_FOUND_TOPIC(500, "TOPIC-ERR-400","추천할 제시어가 없습니다."),
     ALREADY_COMPLETED_POST(500, "POST-ERR-400", "이미 완료된 게시물입니다."),
