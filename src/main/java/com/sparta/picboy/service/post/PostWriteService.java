550fc4b6e2b46bab2ad6e7fc9a1e3c3cfc6252f8
dongkeon-lee <veha357@naver.com>
2022-10-20 02:05:05 +0900

    [update] db잠금 낙관잠금 버전체크

--- src/main/java/com/sparta/picboy/service/post/PostWriteService.java
+++ src/main/java/com/sparta/picboy/service/post/PostWriteService.java
@@ -95,7 +95,7 @@ public class PostWriteService {
 
     // 이어 그리기 생성
 //    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
-    @Lock(value = LockModeType.OPTIMISTIC)
+    @Lock(value = LockModeType.OPTIMISTIC_FORCE_INCREMENT)
     @Transactional
     public ResponseDto<?> relayPost(Long postId, PostDelayRequestDto postDelayRequestDto, UserDetails userinfo) {
         Member member = memberRepository.findByUsername(userinfo.getUsername()).orElse(null);
